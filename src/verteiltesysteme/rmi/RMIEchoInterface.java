60993967e5adc69c01bece3868cab1183a46a73f
Allan <allanvalin@yahoo.com.br>
2019-11-30 18:37:51 +0100

    just the necessary

--- src/verteiltesysteme/rmi/RMIEchoInterface.java
+++
@@ -1,9 +0,0 @@
-package verteiltesysteme.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RMIEchoInterface extends Remote {
-	public String toLowerCase(String input) throws RemoteException;
-	public String toUpperCase(String input) throws RemoteException;
-}
